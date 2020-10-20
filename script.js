b57fc87a7acdf5d669739e3deeb238284a9be8d2
שראל כץ <u8880717@bsmch.net>
2020-10-20 10:30:54 +0300

    changed server.py, index.html, script.js

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 $(function () {
     var socket = io.connect();
 
+    //sockets
+    socket.on('connect', () => {
+        console.log('connected: '+socket.connected);
+        socket.emit('hello', 'world')
+      });
+
     socket.on('hey', function(msg){
-        console.debug(msg)
+        console.log(msg)
+        ctx.font = "30px Arial";
+        ctx.fillText(msg['text'], msg['x'], msg['y']);
     });
+
+    function send_command(command){
+        socket.emit('command', command)
+    }
+
+    //game
+    var canvas = document.getElementById("game_canvas");
+    var ctx = canvas.getContext("2d");
+
+    canvas.addEventListener('keydown', this.get_key, false);
+
+    function get_key(key){
+        var key_code = key.keyCode;
+
+        console.log(key_code)
+
+        if(key_code == 87){
+            send_command('moveup')
+        }
+    }
 });
--- server.py
+++ server.py
@@ -3,7 +3,8 @@ import socketio
 
 static_files = {
     '/': 'index.html',
-    '/script.js': 'script.js'
+    '/script.js': 'script.js',
+    '/style.css': 'style.css'
 }
 
 sio = socketio.Server(async_mode='threading')
@@ -13,7 +14,15 @@ app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app, static_files=static_files)
 @sio.event
 def connect(sid, environ):
     print('sending hey')
-    sio.emit('hey', {'data': 'heyy!!'})
+    sio.emit('hey', {'text': 'heyy!!', 'x': 25, 'y': 50})
+
+@sio.on('hello')
+def hello(sid, *data):
+    print(f'hello {data[0]}')
+
+@sio.on('command')
+def command_handler(sid, *data):
+    print(data)
 
 if __name__ == '__main__':
     app.run(threaded=True)
